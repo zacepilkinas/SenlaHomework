ee57f161890f2a47763ee9dfd1ef0988dd26b98d
zacepilkina <zacepilina02_00@mail.ru>
2022-04-09 18:44:27 +0300

    Empty project

---
+++ SenlaGames/SenlaGames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SenlaGames
+//
+//  Created by Anastasia on 09.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
